763eeace0842feb605dc6f4fe942dcce6151ffc2
Aquib <aquibu1807@gmail.com>
2023-06-09 21:54:30 +0530

    Voice Alert Class Change Function

--- main.js
+++ main.js
@@ -1,80 +1,83 @@
 let ans = Math.floor(Math.random() * 20) + 1;
 
-let guessNumber,
+let Gn,
   score = 20,
   highscore = 0;
-const checkBtnElement = document.getElementsByClassName("check")[0];
-checkBtnElement.addEventListener("click", guessFunction);
-function guessFunction() {
-  guessNumber = document.querySelector(".guess").value;
-  if (Math.abs(guessNumber - ans) == 0) {
-    var audio = new Audio("sounds/victory.mp3");
-    audio.play();
-    $("body").addClass("victory");
-    setTimeout(function () {
-      $("body").removeClass("victory");
-    }, 500);
-    $("#title").text("You are a Winner Mate");
-    document.getElementsByClassName("message")[0].innerHTML = "Correct Number";
-    document.getElementsByClassName("number")[0].innerHTML = ans;
 
+$(".check").on("click", function () {
+  guessFunction();
+});
+function guessFunction() {
+  Gn = $(".guess").val();
+  let val = Gn - ans;
+  if (val === 0) {
+    voiceAlert("victory");
+    classEffect("body", "victory");
+
+    $("#title").text("You are a Winner Mate");
+    $("message").text("Correct Number");
+    $("number").text(ans);
     if (highscore < score) {
       highscore = score;
     }
-  } else if (Math.abs(guessNumber - ans) <= 2 && score != 0) {
-    var audio = new Audio("sounds/very-close.mp3");
-    audio.play();
+  } else if (val <= 2 && score != 0) {
+    voiceAlert("very-close");
     $("#title").text("Too close Keep Trying!!");
-    document.getElementsByClassName("message")[0].innerHTML = "Very Close";
+    $("message").text("Very Close");
     score--;
-  } else if (Math.abs(guessNumber - ans) <= 4 && score != 0) {
-    var audio = new Audio("sounds/close.mp3");
-    audio.play();
+  } else if (val <= 4 && score != 0) {
+    voiceAlert("close");
     $("#title").text("Quite Nice!!");
-    document.getElementsByClassName("message")[0].innerHTML = "Close";
+    $(".message").text("Close");
     score--;
-  } else if (Math.abs(guessNumber - ans) <= 6 && score != 0) {
-    var audio = new Audio("sounds/far.mp3");
-    audio.play();
+  } else if (val <= 6 && score != 0) {
+    voiceAlert("far");
     $("#title").text("A bit Far!!");
-    document.getElementsByClassName("message")[0].innerHTML = "Far";
+    $(".message").text("Far");
     score--;
   } else {
-    var audio = new Audio("sounds/very-far.mp3");
-    audio.play();
+    voiceAlert("very-far");
     $("#title").text("Tooooooo Far!!");
-    document.getElementsByClassName("message")[0].innerHTML = "Very Far";
+    $(".message").text("Very Far");
     if (score != 0) score--;
   }
   if (score == 0) {
-    var audio = new Audio("sounds/wrong.mp3");
-    audio.play();
-    $("body").addClass("game-over");
-    setTimeout(function () {
-      $("body").removeClass("game-over");
-    }, 200);
+    voiceAlert("wrong");
+    classEffect("body", "game-over");
+
     $("#title").text("You Lost");
-    document.getElementsByClassName("message")[0].innerHTML =
-      "You lost the game!";
+    $(".message").text("You lost the game!");
   }
 
-  document.getElementsByClassName("score")[0].innerHTML = score;
-  document.getElementsByClassName("highscore")[0].innerHTML = highscore;
+  $(".score").text(score);
+  $(".highscore").text(highscore);
 }
 
-// const againBtnElement = document.getElementsByClassName("again")[0];
-// againBtnElement.addEventListener("click", refreshFunction);
+//Function to reset Game
 function refreshFunction() {
   ans = Math.floor(Math.random() * 20) + 1;
   score = 20;
-  var audio = new Audio("sounds/game-start.mp3");
-  audio.play();
-  document.getElementsByClassName("message")[0].innerHTML = "Start guessing...";
-  document.getElementsByClassName("number")[0].innerHTML = "?";
+  voiceAlert("game-start");
+
+  $(".message").text("Start guessing...");
+  $(".number").text("?");
   document.body.style.background = "#222";
-  document.getElementsByClassName("score")[0].innerHTML = score;
+  $(".score").text(score);
 }
+
 $(".again").on("click", function () {
   $("#title").text("Guess My Number!");
   refreshFunction();
 });
+
+function voiceAlert(x) {
+  var audio = new Audio("sounds/" + x + ".mp3");
+  audio.play();
+}
+
+function classEffect(element, className) {
+  $(element).addClass(className);
+  setTimeout(function () {
+    $(element).removeClass(className);
+  }, 200);
+}
